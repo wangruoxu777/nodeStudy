746eec4aa81b701c87e12592e09ea1b33b15f366
wrx <wangruoxu@wrx>
2021-02-07 09:28:20 +0800

    提交

--- koa2-tutorial/app.js
+++ koa2-tutorial/app.js
@@ -5,6 +5,6 @@ const middleware = require("./middleware");
 const app = new Koa();
 middleware(app);
 router(app);
-app.listen(3100, () => {
-    console.log("服务器在3100端口启动 http://localhost:3100");
+app.listen(3200, () => {
+    console.log("服务器在3100端口启动 http://localhost:3200");
 });
