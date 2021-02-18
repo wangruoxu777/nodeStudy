90ab3de9c9fbf8a9d479f014d595ef08b7742da7
wangruoxu777 <wangruoxu777@163.com>
2021-02-18 22:30:09 +0800

    feat: 春节代码

--- koa2-tutorial/controller/home.js
+++ koa2-tutorial/controller/home.js
@@ -1,30 +1,32 @@
-const HomeService = require("../service/home");
-
 module.exports = {
     index: async (ctx, next) => {
-        await ctx.render("home/index", { title: "zhangsan欢迎您" });
+        await ctx.render("home/index", { title: "iKcamp欢迎您" })
     },
     home: async (ctx, next) => {
-        console.log(ctx.request.query);
-        console.log(ctx.request.querystring);
-        console.log(ctx.params);
-        ctx.body = `<h1>Home page /:id /:name</h1>`;
+        console.log(ctx.request.query)
+        console.log(ctx.request.querystring)
+        ctx.response.body = '<h1>HOME page</h1>'
+    },
+    homeParams: async (ctx, next) => {
+        console.log(ctx.params)
+        ctx.response.body = '<h1>HOME page /:id/:name</h1>'
     },
     login: async (ctx, next) => {
-        await ctx.render("home/login", {
-            btnName: "GoGoGo",
-        });
+        await ctx.render('home/login', {
+            btnName: 'GoGoGo'
+        })
     },
     register: async (ctx, next) => {
-        let params = ctx.request.body;
-        let name = params.name;
-        let password = params.password;
-        let res = await HomeService.register(name, password);
-        if (res.status === "-1") {
-            await ctx.render("home/login", res.data);
+        const { app } = ctx
+        let params = ctx.request.body
+        let name = params.name
+        let password = params.password
+        let res = await app.service.home.register(name, password)
+        if (res.status == "-1") {
+            await ctx.render("home/login", res.data)
         } else {
-            ctx.state.title = "个人中心";
-            await ctx.render("home/success", res.data);
+            ctx.state.title = "个人中心"
+            await ctx.render("home/success", res.data)
         }
-    },
-};
+    }
+}
